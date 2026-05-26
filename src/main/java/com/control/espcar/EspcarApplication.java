f25003c97dc5c1b280ab536d0bc544994ec9adbd
Triệu Long 9999 <hotrokhachhang3211@gmail.com>
2026-05-27 00:02:24 +0700

    first commit

---
+++ src/main/java/com/control/espcar/EspcarApplication.java
@@ -0,0 +1,13 @@
+package com.control.espcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/control/espcar/EspcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control.espcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
